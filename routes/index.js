e74b140fefc2cb66c3f73aafc117d5fa30828519
gresba <kimpaul0709@gmail.com>
2022-07-28 13:15:56 -0400

    Added Routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
+import usersRoutes from "./users.js";
+import commentsRoutes from "./comments.js";
+import picturesRoutes from "./pictures.js";
 
 const router = Router();
 
 router.get("/", (req, res) => res.send("Api Root"));
+
+router.use("/", usersRoutes);
+router.use("/", commentsRoutes);
+router.use("/", picturesRoutes);
 
 export default router;
